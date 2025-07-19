3f97ee9eafcb3c0bec132d55a0d9d28525fd1fa9
Ken <ken.chen8176@gmail.com>
2025-07-19 16:56:45 -0400

    set defaults

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { ChakraProvider, CSSReset, Box } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  CSSReset,
+  Box,
+  ColorModeScript,
+} from "@chakra-ui/react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,7 +12,6 @@ import {
   Navigate,
 } from "react-router-dom";
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
-import { isUserVerified, checkFirestoreVerification } from "./utils/auth";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
@@ -58,49 +62,52 @@ const RootRoute = () => {
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <CSSReset />
-      <Router>
-        <AuthProvider>
-          <Navbar />
-          <Box pt="72px">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route
-                path="/email-verification"
-                element={<EmailVerification />}
-              />
-              <Route
-                path="/dashboard"
-                element={
-                  <PrivateRoute>
-                    <Dashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/leaderboard"
-                element={
-                  <GuestRoute>
-                    <Leaderboard />
-                  </GuestRoute>
-                }
-              />
-              <Route
-                path="/trading"
-                element={
-                  <VerifiedRoute>
-                    <Trading />
-                  </VerifiedRoute>
-                }
-              />
-              <Route path="/" element={<RootRoute />} />
-            </Routes>
-          </Box>
-        </AuthProvider>
-      </Router>
-    </ChakraProvider>
+    <>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <ChakraProvider theme={theme}>
+        <CSSReset />
+        <Router>
+          <AuthProvider>
+            <Navbar />
+            <Box pt="72px">
+              <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route
+                  path="/email-verification"
+                  element={<EmailVerification />}
+                />
+                <Route
+                  path="/dashboard"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/leaderboard"
+                  element={
+                    <GuestRoute>
+                      <Leaderboard />
+                    </GuestRoute>
+                  }
+                />
+                <Route
+                  path="/trading"
+                  element={
+                    <VerifiedRoute>
+                      <Trading />
+                    </VerifiedRoute>
+                  }
+                />
+                <Route path="/" element={<RootRoute />} />
+              </Routes>
+            </Box>
+          </AuthProvider>
+        </Router>
+      </ChakraProvider>
+    </>
   );
 }
 
